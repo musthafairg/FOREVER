750cd57ec65d92dac0aa174aa5939e3fb5bb148b
musthafairg <musthafairg786@gmail.com>
2025-12-06 12:19:56 +0530

    project initialized

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import 'dotenv/config'
+import session from 'express-session'
+import nocache from 'nocache'
+import connectDB from './config/mongodb.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import userRouter from './routes/user.js'
+import adminRouter from './routes/admin.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const app = express()
+const port =process.env.PORT||4000
+connectDB()
+
+
+app.use(nocache())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000*60*60*24 ,
+      httpOnly: true,
+      secure: false,
+    },
+}))
+
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use('/',userRouter)
+app.use('/admin',adminRouter)
+
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+    
+})
