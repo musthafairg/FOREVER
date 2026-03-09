8606a9c06c380c65e4c951c7c8324348b92836fb
musthafairg <musthafairg786@gmail.com>
2026-03-09 11:28:05 +0530

    fix : add eye icon to view password in edit page

--- public/js/edit-password.js
+++ public/js/edit-password.js
@@ -74,3 +74,20 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
   }
 });
+document.querySelectorAll(".toggle-password").forEach(icon => {
+
+  icon.addEventListener("click", () => {
+
+    const input = document.getElementById(icon.dataset.target);
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+
+  });
+
+});
